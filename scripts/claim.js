ba77daa86d0b3de660d623cc81bbdde4d66992e8
roomservice <roomservice@yourbtc.net>
2020-05-12 23:39:09 +0200

    add 10sec delay when claiming A LOT OF accounts at once

--- scripts/claim.js
+++ scripts/claim.js
@@ -1,36 +1,42 @@
-const { Client, PrivateKey, Asset } = require('dsteem');
+const { Client, PrivateKey, Asset } = require("dsteem");
 
-var fs = require('fs')
+var fs = require("fs");
 
-var config = JSON.parse(fs.readFileSync('config.json'))
+var config = JSON.parse(fs.readFileSync("config.json"));
 
 const client = new Client(config.rpc);
 
-var wif = config.key
-var account = config.account
+var wif = config.key;
+var account = config.account;
 
 module.exports = {
-  claimAccount: function claimAccount(){
-  const op = ['claim_account', {
-	creator: account,
-	fee: Asset.from('0.000 STEEM'),
-	extensions: [],
-  }];
-  claim(op)
-  setInterval(() => {
-    claim(op)
-  }, 1000*60*60*12) //12h
+  claimAccount: function claimAccount() {
+    const op = [
+      "claim_account",
+      {
+        creator: account,
+        fee: Asset.from("0.000 STEEM"),
+        extensions: [],
+      },
+    ];
+    claim(op);
+    setInterval(() => {
+      claim(op);
+    }, 1000 * 60 * 60 * 12); //12h
 
-	function claim(op){
-    console.log('Claiming account...')
-  	client.broadcast.sendOperations([op], PrivateKey.from(wif))
-			.then((res) => {
-			 console. log('You have successfully claimed a discounted account');
-       claim(op)
-			})
-			.catch(err => {
-			  console.log("Error claiming account...");
-			});
-  	}
-  }
-}
+    function claim(op) {
+      console.log("Claiming account...");
+      client.broadcast
+        .sendOperations([op], PrivateKey.from(wif))
+        .then((res) => {
+          console.log("You have successfully claimed a discounted account");
+          setInterval(() => {
+            claim(op);
+          }, 10000);
+        })
+        .catch((err) => {
+          console.log("Error claiming account...");
+        });
+    }
+  },
+};
